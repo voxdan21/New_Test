bfc1d1841aa38b9587e07d340c1c71d981d10af6
Voxdan21 <popov.daniil21@gmail.com>
2022-08-30 22:49:47 +0300

    Example_4

---
+++ Example_4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введи число: ");
+int number = int.Parse(Console.ReadLine());
+int startNumber = Math.Abs(number) * -1;
+while (startNumber <= number)
+{
+    Console.WriteLine(startNumber);
+    startNumber = startNumber + 1;
+} 
