5751dbfe30c46544d731ac003ad1c7c7e937945e
Voxdan21 <popov.daniil21@gmail.com>
2022-08-30 22:45:22 +0300

    Example_1

---
+++ Example_1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введи число: ");
+int number = int.Parse(Console.ReadLine());
+int result = number*number;
+Console.WriteLine("Квадрат числа " + number + " = " + result);
+Console.WriteLine($"Квадрат числа  {number} = {result}");
+
+int result_2 = Convert.ToInt32(Math.Pow(number, 2));
+Console.WriteLine($"Квадрат числа  {number} = {result_2}");
