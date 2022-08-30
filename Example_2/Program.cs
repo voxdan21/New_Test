599eccd34eba9ebfb823ad3f2e5e8693992d38e6
Voxdan21 <popov.daniil21@gmail.com>
2022-08-30 22:47:52 +0300

    Example_2

---
+++ Example_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введи число №1: ");
+int number_1 = int.Parse(Console.ReadLine());
+Console.Write("Введи число №2: ");
+int number_2 = int.Parse(Console.ReadLine());
+int result = Convert.ToInt32(Math.Pow(number_2, 2));
+if  (result == number_1) 
+{
+Console.WriteLine("Да");
+}
+else 
+{
+Console.WriteLine("Нет");
+}
