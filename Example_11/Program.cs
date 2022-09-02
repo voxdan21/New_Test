527a20f774e085f7251386b39d899b056b1262b7
Voxdan21 <popov.daniil21@gmail.com>
2022-09-02 22:05:29 +0300

    11

---
+++ Example_11/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+Console.Write("Введите  число: ");
+int num = int.Parse(Console.ReadLine());
+
+void SecondNumber(int num){
+while (num > 100)
+{num = num/10;}
+int result = num%10;
+Console.WriteLine(result);
+}
+
+SecondNumber(num);
