a3545eab5b8f4024a88d5618e5867101f43d2e6b
Voxdan21 <popov.daniil21@gmail.com>
2022-09-02 22:07:14 +0300

    11

--- Example_11/Program.cs
+++ Example_11/Program.cs
@@ -3,11 +3,12 @@
 Console.Write("Введите  число: ");
 int num = int.Parse(Console.ReadLine());
 
-void SecondNumber(int num){
+void SecondNumber(int num1){
+
 while (num > 100)
 {num = num/10;}
 int result = num%10;
-Console.WriteLine(result);
+Console.WriteLine($"Вторая цифра в числе {num1} = {result}");
 }
 
 SecondNumber(num);
