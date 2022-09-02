c0a1989815557fed79ca5cb7f4098653a087a52a
Voxdan21 <popov.daniil21@gmail.com>
2022-09-02 23:14:05 +0300

    Добавил отрицательные и 0

--- Example_12/Program.cs
+++ Example_12/Program.cs
@@ -1,66 +1,75 @@
-﻿void SecondNumber(int a){
-int num = a;
-
-if(a<10 && a>=0){
-    Console.WriteLine("Нет второй цифры");
-    }
-
-else if (a<0){
-    num=num*-1;
-    while (num > 100)
-{num = num/10;}
-int result = num%10;
-if(a<10 && a>=0){
-    Console.WriteLine("Нет второй цифры");
-    }
-
-Console.WriteLine($"Вторая цифра в числе {a} = {result}");
-}
-
-else
+﻿void SecondNumber(int a)
 {
-while (num > 100)
-{num = num/10;}
-int result = num%10;
-if(a<10 && a>=0){
-    Console.WriteLine("Нет второй цифры");
+    int num = a;
+
+    if (a < 10 && a >= 0)
+    {
+        Console.WriteLine("Нет второй цифры");
     }
 
-Console.WriteLine($"Вторая цифра в числе {a} = {result}");
+    else if (a < 0)
+    {
+        num = num * -1;
+        while (num > 100)
+        { num = num / 10; }
+        int result = num % 10;
+        if (a < 10 && a >= 0)
+        {
+            Console.WriteLine("Нет второй цифры");
+        }
+
+        Console.WriteLine($"Вторая цифра в числе {a} = {result}");
+    }
+
+    else
+    {
+        while (num > 100)
+        { num = num / 10; }
+        int result = num % 10;
+        if (a < 10 && a >= 0)
+        {
+            Console.WriteLine("Нет второй цифры");
+        }
+
+        Console.WriteLine($"Вторая цифра в числе {a} = {result}");
+    }
 }
+
+void NumberWithoutSecond(int a)
+{
+    int num = a;
+    string num2 = "";
+    if (a < 10 && a >= 0) { Console.WriteLine($"Число без второй цифры:{a}"); }
+    else if (a < 0)
+    {
+        num = num * -1;
+        while (num > 100)
+        {
+
+            num2 = num % 10 + num2;
+            num = num / 10;
+        }
+
+        num = num / 10;
+        num2 = "-" + num + num2;
+
+        Console.WriteLine($"Число без второй цифры:{num2}");
+    }
+    else
+    {
+        while (num > 100)
+        {
+
+            num2 = num % 10 + num2;
+            num = num / 10;
+        }
+
+        num = num / 10;
+        num2 = num + num2;
+
+        Console.WriteLine($"Число без второй цифры:{num2}");
+    }
 }
-
-void NumberWithoutSecond(int a){
-   int num = a; 
-  string num2 ="";
- if(a<10 && a>=0){Console.WriteLine($"Число без второй цифры:{a}");}
- else if (a<0)
- {
- num=num*-1;
- while (num>100){
-
-    num2 = num%10 +num2;
-    num = num/10;
- }  
-
- num = num/10;
- num2 = "-"+ num+num2;
- 
- Console.WriteLine($"Число без второй цифры:{num2}");
- }
- else{
- while (num>100){
-
-    num2 = num%10 +num2;
-    num = num/10;
- }  
-
- num = num/10;
- num2 = num+num2;
- 
- Console.WriteLine($"Число без второй цифры:{num2}");
- }
- }
 
 
 Console.Write("Введите  число: ");
