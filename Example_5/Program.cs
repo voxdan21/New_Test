defa9ce69a7621168b37bfb787f5d4c9595b65f4
Voxdan21 <popov.daniil21@gmail.com>
2022-09-02 20:20:11 +0300

    5

---
+++ Example_5/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write ("Введите целое число ");
+            try
+                {
+                int x=Convert.ToInt32 (Console.ReadLine());
+ 
+                Console.WriteLine(x*10);
+
+                }
+            catch 
+            {
+                
+                Console.WriteLine("надо было вводить именно целое число");
+            }
