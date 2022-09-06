d616967df3c887563c41ec4204549365defd2c7c
Voxdan21 <popov.daniil21@gmail.com>
2022-09-06 20:30:51 +0300

    добавили задачу 13

---
+++ Example_13/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] nums = new int[10]{10,45,23,45,65,23,43,56,23,23};
+int max = nums[0];
+int count = 0;
+for (int i = 0; i < 10; i++)
+{
+    if(nums[i]>max)
+    {
+     max = nums[i]; 
+    
+    }
+}
+
+Console.WriteLine(max);
