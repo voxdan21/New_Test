d34635253620250ed62ee69345838456eeb614b4
Voxdan21 <popov.daniil21@gmail.com>
2022-09-06 21:18:56 +0300

    добавили задачу 15

---
+++ Example_15/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 18: Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+
+string Diaposon(int number)
+{
+    if (number == 1)
+    {
+        return "x>0,y>0";
+    }
+    if (number == 2)
+    {
+        return "x<0,y>0";
+    }
+    if (number == 3)
+    {
+        return "x<0,y<0";
+    }
+    if (number == 4)
+    {
+        return "x>0,y<0";
+    }
+    else
+        return "mistake";
+}
+
+
+
+Console.Write("Input Area: ");
+int Area = int.Parse(Console.ReadLine());
+Console.WriteLine(Diaposon(Area));
