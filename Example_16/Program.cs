526e40b73124c178cfd1f26e96bd5d553a13e37c
Voxdan21 <popov.daniil21@gmail.com>
2022-09-06 21:57:35 +0300

    добавили задачу 16

--- Example_16/Program.cs
+++ Example_16/Program.cs
@@ -1,9 +1,9 @@
 ﻿// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
 
-double GetDistance (int x1, int y1, int x2, int y2)
-{   
-    double distance = Math.Sqrt(Math.Pow((x2-x1),2)+ Math.Pow((y2-y1),2));
-    return Math.Round(distance,2,MidpointRounding.ToNegativeInfinity);
+double GetDistance(int x1, int y1, int x2, int y2)
+{
+    double distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+    return Math.Round(distance, 2, MidpointRounding.ToNegativeInfinity);
 }
 
 
@@ -21,4 +21,4 @@ int x1 = int.Parse(Console.ReadLine());
 Console.Write("Input Y(2 number): ");
 int y1 = int.Parse(Console.ReadLine());
 
-Console.WriteLine(GetDistance(x,y,x1,y1));
+Console.WriteLine(GetDistance(x, y, x1, y1));
