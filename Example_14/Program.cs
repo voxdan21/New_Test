03e8f295ea237d26c37d9d73474eff62fdf21b9b
Voxdan21 <popov.daniil21@gmail.com>
2022-09-06 21:04:54 +0300

    добавили задачу 14

---
+++ Example_14/Program.cs
@@ -0,0 +1,33 @@
+﻿//Определение четверти точки
+int GetPointArea(int x, int y)
+{
+    if (x > 0 && y > 0)
+    {
+        return 1;
+    }
+    if (x < 0 && y > 0)
+    {
+        return 2;
+    }
+    if (x < 0 && y < 0)
+    {
+        return 3;
+    }
+    if (x > 0 && y < 0)
+    {
+        return 4;
+    }
+    return 0;
+}
+
+
+
+
+Console.Write("Input X: ");
+int x = int.Parse(Console.ReadLine());
+
+Console.Write("Input Y: ");
+int y = int.Parse(Console.ReadLine());
+
+int areaNum = GetPointArea(x,y);
+Console.WriteLine($"Area= {areaNum}");
