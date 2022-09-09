8638f4269cdfc462d4f6bc3f76f734dd22208d93
Voxdan21 <popov.daniil21@gmail.com>
2022-09-09 20:42:06 +0300

    добавили 18

---
+++ Example_18/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+
+
+int GetSum(int number)
+{
+    int sum = 0;
+    if (number > 0)
+    {
+        for (int i = 1; i <= number; i++)
+        {
+            sum = sum + i;
+        }
+    }
+    if (number < 0)
+    {
+        for (int i = 1; i >= number; i--)
+        {
+            sum = sum + -i;
+        }
+    }
+    return sum;
+}
+
+
+
+Console.Write("Input A:  ");
+
+try
+{
+    int a = int.Parse(Console.ReadLine());
+    int sum = GetSum(a);
+    Console.WriteLine(sum);
+}
+catch (Exception ex)
+{
+ Console.WriteLine($"Входное значение имеет неверный формат. Ошибка: {ex.Message}");
+}
+
