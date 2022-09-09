c2876e95f3abf1cb40b67d0e6cbf60ce5cb07326
Voxdan21 <popov.daniil21@gmail.com>
2022-09-09 21:15:19 +0300

    добавили 20

---
+++ Example_20/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+int GetProizved(int number)
+{
+    int multiplication = 1;
+    if (number > 0)
+    {
+        for (int i = 1; i <= number; i++)
+        {
+            multiplication = multiplication * i;
+        }
+    }
+    if (number < 0)
+    {
+        for (int i = 1; i >= number; i--)
+        {
+            multiplication = multiplication * i;
+        }
+    }
+    return multiplication;
+}
+
+
+
+Console.Write("Input A:  ");
+
+try
+{
+    int a = int.Parse(Console.ReadLine());
+    int sum = GetProizved(a);
+    Console.WriteLine(sum);
+}
+catch (Exception ex)
+{
+ Console.WriteLine($"Входное значение имеет неверный формат. Ошибка: {ex.Message}");
+}
