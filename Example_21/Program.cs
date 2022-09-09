caf2988cf9da57ddc30a3d8646891966b83df50d
Voxdan21 <popov.daniil21@gmail.com>
2022-09-09 21:33:55 +0300

    добавили 21

---
+++ Example_21/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+string NewRandomeMassive()
+{
+    int[] random;
+    random = new int[7];
+    string Mass = string.Empty;
+    for (int i = 0; i < 7; i++)
+    {
+        random[i] = new Random().Next(0,2);
+        Mass = Mass + random[i];
+    }
+    return Mass;
+}
+
+string Result = NewRandomeMassive();
+Console.WriteLine(Result);
