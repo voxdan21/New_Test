1228fa1f79ccd933fe3ceda1428148d4005eb9c6
Voxdan21 <popov.daniil21@gmail.com>
2022-09-13 21:55:03 +0300

    добавили 24

--- Example_24/Program.cs
+++ Example_24/Program.cs
@@ -2,32 +2,32 @@
 // 4; массив [6, 7, 19, 345, 3] -> нет
 // 3; массив [6, 7, 19, 345, 3] -> да
 
-bool Metod (int [] array, int number)
+bool Metod(int[] array, int number)
 {
     bool result = false;
     for (int i = 0; i < array.Length; i++)
     {
-       if (array [i] == number) 
-       {
+        if (array[i] == number)
+        {
             result = true;
             break;
-       }
-       else result = false;
-       
+        }
+        else result = false;
+
     }
     return result;
 }
 
 try
 {
-    Console.Write ("Введите число: ");
-    int number = Convert.ToInt32 (Console.ReadLine ()); 
-    int [] array = {5, -4, 5, -1, 0};
-    var result = Metod (array, number);
+    Console.Write("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int[] array = { 5, -4, 5, -1, 0 };
+    var result = Metod(array, number);
     Console.WriteLine(result);
 }
 
 catch
 {
-    Console.Write ("Ошибка: не верно введены данные.");
+    Console.Write("Ошибка: не верно введены данные.");
 }
