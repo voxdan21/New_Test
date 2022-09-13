e5161fca8da7270968369d134c300c5312205908
Voxdan21 <popov.daniil21@gmail.com>
2022-09-13 21:13:05 +0300

    добавили 23

--- Example_23/Program.cs
+++ Example_23/Program.cs
@@ -1,31 +1,31 @@
 ﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
 // [-4, -8, 8, 2] -> [4, 8, -8, -2]
-void PrintArray (int [] array)
+void PrintArray(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write ($"{array [i]} ");
+        Console.Write($"{array[i]} ");
     }
     return;
 }
 
-int [] Metod (int [] array)
+int[] Metod(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
-       array [i] = array [i] * -1;
+        array[i] = array[i] * -1;
     }
     return array;
 }
 
 try
 {
-    int [] array = {5, -4, 5, -1, 0};
-    int [] newArray = Metod (array);
-    PrintArray (newArray);
+    int[] array = { 5, -4, 5, -1, 0 };
+    int[] newArray = Metod(array);
+    PrintArray(newArray);
 }
 
 catch
 {
-    Console.Write ("Ошибка: не верно введены данные.");
+    Console.Write("Ошибка: не верно введены данные.");
 }
