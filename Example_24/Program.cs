a623553fc8c13b5ecb3ef705f8632524f948fe16
Voxdan21 <popov.daniil21@gmail.com>
2022-09-13 21:11:27 +0300

    Задание №24

---
+++ Example_24/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+bool Metod (int [] array, int number)
+{
+    bool result = false;
+    for (int i = 0; i < array.Length; i++)
+    {
+       if (array [i] == number) 
+       {
+            result = true;
+            break;
+       }
+       else result = false;
+       
+    }
+    return result;
+}
+
+try
+{
+    Console.Write ("Введите число: ");
+    int number = Convert.ToInt32 (Console.ReadLine ()); 
+    int [] array = {5, -4, 5, -1, 0};
+    var result = Metod (array, number);
+    Console.WriteLine(result);
+}
+
+catch
+{
+    Console.Write ("Ошибка: не верно введены данные.");
+}
