2e485da20b1977afbd1c805ba407222871cf5548
Voxdan21 <popov.daniil21@gmail.com>
2022-09-16 20:45:58 +0300

    Задание №28

---
+++ Example_28/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 39: Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+void PrintArray (int[]array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write ($"{array[i]} ");
+}
+}
+
+
+int [] GetReverseArray (int[] array)
+{
+    int [] result = new int[array.Length];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = array[array.Length-1-i];
+    }
+    return result;
+}
+
+int[] array = new int[] { 1, 4, 6, 3, 7, 9 , 8};
+int[] result = GetReverseArray(array);
+PrintArray(result);
