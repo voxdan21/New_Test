9481487a53aa207c13bd7ac63753533307e476f9
Voxdan21 <popov.daniil21@gmail.com>
2022-09-16 21:21:14 +0300

    Задание №30

---
+++ Example_30/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+
+string GetDecimalFromStingInt (string fakeInt)
+{
+    string fakeDecimal = null ;
+    int i = int.Parse(fakeInt);
+    while ( i != 0 )
+    {
+        int temp = i%2;
+        fakeDecimal = (temp+ fakeDecimal);
+        i /= 2; 
+    }
+    return fakeDecimal;
+}
+
+Console.WriteLine(GetDecimalFromStingInt("45"));
